a053b3db5c2106ed08b4f29b10852c6efc406028
swatipanchal64 <swatipanchal64@gmail.com>
2023-10-21 19:56:14 -0400

    Added the area and is inside the triangle or not

--- Test.java
+++ Test.java
@@ -4,9 +4,11 @@ public class Test {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        Triangle[] t = new Triangle[3];
+        int size = 3;
 
-        for(int i = 0; i < 1; i++) {
+        Triangle[] t = new Triangle[size];
+
+        for(int i = 0; i < size; i++) {
 
             System.out.printf("-------------Triangle %d------------- \n", (i+1));
 
@@ -25,28 +27,30 @@ public class Test {
             }
 
              t[i] = new Triangle(p[0], p[1], p[2]);
+
+             System.out.println("The given triangle is "+t[i].findType());
+             System.out.printf("The perimeter for the given triangle is %.2f\n", t[i].findPerimeter());
         }
 
-//        System.out.print("Enter the x co-ordinate of point to be checked: ");
-//        double pointX = in.nextDouble();
-//
-//        System.out.print("Enter the y co-ordinate of point to be checked: ");
-//        double pointY = in.nextDouble();
-//
+        System.out.print("Enter the x co-ordinate of point to be checked: ");
+        double pointX = in.nextDouble();
+
+        System.out.print("Enter the y co-ordinate of point to be checked: ");
+        double pointY = in.nextDouble();
+
+        Point p1 = new Point(pointX, pointY);
+
         int i = 1;
         for (Triangle triangle : t) {
 
-            System.out.println(triangle.findType());
+            boolean isInside = triangle.isInside(p1);
 
-
-//            boolean isInside = circle.isInternal(pointX, pointY);
-//
-//            if (isInside) {
-//                System.out.printf("The point is inside the circle %d\n", i++);
-//            }
-//            else {
-//                System.out.printf("The point is outside the circle %d\n", i++);
-//            }
+            if (isInside) {
+                System.out.printf("The point is inside the triangle %d\n", i++);
+            }
+            else {
+                System.out.printf("The point is outside the triangle %d\n", i++);
+            }
         }
 
     }
--- Triangle.java
+++ Triangle.java
@@ -1,10 +1,14 @@
 public class Triangle {
     private Point a, b, c;
+    private double side1, side2, side3;
 
     public Triangle(Point a, Point b, Point c) {
         this.a = a;
         this.b = b;
         this.c = c;
+        this.side1 = findDistance(this.a, this.b);
+        this.side2 = findDistance(this.b, this.c);
+        this.side3 = findDistance(this.c, this.a);
     }
 
     public double findDistance(Point a, Point b) {
@@ -13,19 +17,42 @@ public class Triangle {
 
     public Type findType() {
 
-        double side1 = findDistance(this.a, this.b);
-        double side2 = findDistance(this.b, this.c);
-        double side3 = findDistance(this.c, this.a);
-
-        if (side1 == side2 && side2 == side3) {
+        if (this.side1 == this.side2 && this.side2 == this.side3) {
             return Type.Equilateral;
         }
-        else if(side1 == side2 || side1 == side3 || side2 == side3) {
+        else if(this.side1 == this.side2 || this.side1 == this.side3 || this.side2 == this.side3) {
             return Type.Isosceles;
         }
         else {
             return Type.Unknown;
         }
+    }
+
+    public double findPerimeter() {
+        return  this.side1 + this.side2 + this.side3;
+    }
+
+    public double area(double x1, double y1, double x2, double y2, double x3, double y3)
+    {
+        return Math.abs((x1*(y2-y3) + x2*(y3-y1)+ x3*(y1-y2))/2.0);
+    }
+
+    public boolean isInside(Point p)
+    {
+        /* Calculate area of triangle ABC */
+        double A = area (this.a.getX(), this.a.getY(), this.b.getX(), this.b.getY(), this.c.getX(), this.c.getY());
+
+        /* Calculate area of triangle PBC */
+        double A1 = area (p.getX(), p.getY(), this.b.getX(), this.b.getY(), this.c.getX(), this.c.getY());
+
+        /* Calculate area of triangle PAC */
+        double A2 = area (this.a.getX(), this.a.getY(), p.getX(), p.getY(), this.c.getX(), this.c.getY());
+
+        /* Calculate area of triangle PAB */
+        double A3 = area (this.a.getX(), this.a.getY(), this.b.getX(), this.b.getY(), p.getX(), p.getY());
+
+        /* Check if sum of A1, A2 and A3 is same as A */
+        return (A == A1 + A2 + A3);
     }
 
 }
